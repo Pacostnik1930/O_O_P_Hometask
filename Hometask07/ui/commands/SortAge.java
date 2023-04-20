705cc54369b9769f21248c9b218fe8633f534952
Pacostnik1930 <118471296+Pacostnik1930@users.noreply.github.com>
2023-04-20 12:37:08 +0300

    Create SortAge.java

---
+++ Hometask07/ui/commands/SortAge.java
@@ -0,0 +1,21 @@
+public class SortAge extends Command
+{
+    public SortAge(Console console) 
+    {
+        super(console);
+    }
+
+    @Override
+    public String description() 
+    {
+        return "Сортировка семьи по возрасту. ";
+    }
+
+    @Override
+    public void execute() 
+    {
+        getConsole().sortFamilyAge();
+        
+    }
+    
+}
