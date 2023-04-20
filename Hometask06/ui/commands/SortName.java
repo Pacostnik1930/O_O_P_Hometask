8bb604b3f62faf84c7243cb6558f34af045aa36d
Pacostnik1930 <118471296+Pacostnik1930@users.noreply.github.com>
2023-04-20 12:34:16 +0300

    Create SortName.java

---
+++ Hometask06/ui/commands/SortName.java
@@ -0,0 +1,19 @@
+public class SortName extends Command
+{
+    public SortName(Console console) 
+    {
+        super(console);
+    }
+
+    @Override
+    public String description() 
+    {
+        return "Сортировка семьи по имени. ";
+    }
+
+    @Override
+    public void execute() 
+    {
+        getConsole().sortFamilyName();     
+    }    
+}
